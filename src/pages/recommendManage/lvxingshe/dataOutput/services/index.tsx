ae3bb9e07ec20084dd1e3035e7ad2a485be1fb13
wangkuan1994 <1913992554@qq.com>
2019-06-21 12:55:24 +0800

    生产bug修复

--- src/pages/recommendManage/lvxingshe/dataOutput/services/index.tsx
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request';
-
-export function productTasklist(
-  query: {
-    
-  }
-) {
-  return request.get(`http://unitest.fosunholiday.com/api/mms/spread/backend/accounts/export/prize/issue`,query);
-}
